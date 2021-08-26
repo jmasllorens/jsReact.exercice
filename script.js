c116bea0e097a288df933b6da67625ae26b2cade
Jael Masllorens <jmasllorens@gmail.com>
2021-08-26 23:47:08 +0200

    Creation of basic structure html/js

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('buh')
+
+let again = 'soy una variable let' // TypeScript: let again: String = 'soy una variable let'
+const forever = 'soy una variable constante'
+var iz = 'soy una variable variz'
+const list = []
+const fullList = [2, 0]
+const anotherList = list.concat(9)
+
+console.log(forever.length)
+again = again.toUpperCase()
+list.push(0, 2, 6)
+fullList.push(-2)
+console.log(forever.toUpperCase(), again, list, fullList, list[0], fullList[0], anotherList)
